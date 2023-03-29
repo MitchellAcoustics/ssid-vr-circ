f62da6e0e6f28ffdfb17ccd5a6d968bc7a3d4699
Andrew Mitchell <mitchellacoustics15@gmail.com>
2023-03-29 13:56:02 +0100

    fix path bug

--- ssid_vr_circ/main.py
+++ ssid_vr_circ/main.py
@@ -1,5 +1,6 @@
 import argparse
 from ssid_vr_circ import process
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Process circumplex data from SSID VR Circumplex Study')
 parser.add_argument('circ_dir', type=str, help='Directory containing circumplex data')
@@ -7,6 +8,10 @@ parser.add_argument('--index_file', type=str, default='SSID IVR STUDY 1 EXPERIME
 parser.add_argument('--out_file', type=str, default=None, help='Output file name')
 args = parser.parse_args()
 
-process.save_circ_data(args.circ_dir, args.out_file, args.index_file)
+circ_dir = Path(args.circ_dir)
+out_file = Path(args.out_file) if args.out_file else None
+index_file = Path(args.index_file) if args.index_file else None
+
+process.save_circ_data(circ_dir, out_file, index_file)
 
 
